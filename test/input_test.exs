02beb3d43769e09f52481cc795fe084f0f134388
Jessica Chung <jchung722@gmail.com>
2019-04-17 11:12:49 -0500

    update input module tests

--- test/input_test.exs
+++ test/input_test.exs
@@ -3,8 +3,18 @@ defmodule InputTest do
   doctest Input
 
   describe "when input is valid" do
-    test "user input is stripped of trailing newline" do
+    test "user input is stripped of trailing new line" do
       assert Input.trim("1\n") == "1"
+    end
+  end
+
+  describe "when input is invalid" do
+    test "an empty input will be stripped of new  line and be empty" do
+      assert Input.trim("\n") == ""
+    end
+
+    test "an invalid input will be stripped of trailing new line" do
+      assert Input.trim("invalid\n") == "invalid"
     end
   end
 end
