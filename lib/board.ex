fbc4d1524d35a28996eccfb5d5de3ca5f305ab7a
Jessica Chung <jchung722@gmail.com>
2019-04-30 11:43:26 -0500

    refactor format board method

--- lib/board.ex
+++ lib/board.ex
@@ -1,15 +1,9 @@
 defmodule Board do
   defstruct spots: ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-  def format([]), do: ""
-
-  def format([ a, b, c | tail]) do
-    cond do
-      tail == [] ->
-        " #{a} | #{b} | #{c} "
-      true ->
-        " #{a} | #{b} | #{c} \n---+---+---\n"  <> format(tail)
-    end
+  def format(board) do
+    as_symbols(board) |>
+    arrange_string()
   end
 
   def update(move_as_index, board, player) do
@@ -19,4 +13,28 @@ defmodule Board do
   def display(formatted_board) do
     IO.puts(formatted_board)
   end
+
+  defp as_symbols([]), do: []
+
+  defp as_symbols([head | tail]) do
+    cond do
+      is_map(head) ->
+        [ head.symbol | as_symbols(tail)]
+      true ->
+        [head | as_symbols(tail)]
+    end
+  end
+
+  defp arrange_string([]), do: ""
+
+  defp arrange_string([ a, b, c | tail]) do
+
+    cond do
+      tail == [] ->
+        " #{a} | #{b} | #{c} "
+      true ->
+        " #{a} | #{b} | #{c} \n---+---+---\n"  <> format(tail)
+    end
+  end
+
 end
